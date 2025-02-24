5c8620ae80fdb834df27c8129f28df35005eb452
Dave Boyle <spektraldevelopment@gmail.com>
2025-02-24 11:12:00 -0500

    [named-exports] - Troubleshooting.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import * as string from "./string";
-import * as url from "./url";
-import * as cookie from "./cookie";
-import * as dom from "./dom";
-import * as object from "./object";
-import * as form from "./form";
+import * as stringUtils from "./string";
+import * as urlUtils from "./url";
+import * as cookieUtils from "./cookie";
+import * as domUtils from "./dom";
+import * as objectUtils from "./object";
+import * as formUtils from "./form";
+
+export const cookie = cookieUtils;
+export const dom = domUtils;
+export const form = formUtils;
+export const object = objectUtils;
+export const string = stringUtils;
+export const url = urlUtils;
 
 const SpektralUtils = {
   cookie,
