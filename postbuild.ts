eec9e5217253fb13fd5bc8113b7fe4e3a370b7c6
Dave Boyle <spektraldevelopment@gmail.com>
2025-02-25 15:15:37 -0500

    [main] - Troubleshooting postbuild script.

--- postbuild.ts
+++ postbuild.ts
@@ -1,35 +1,48 @@
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
-import { exec, execSync } from "child_process";
+import { execSync } from "child_process";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename: string  = fileURLToPath(import.meta.url);
+const __dirname: string = path.dirname(__filename);
 
 const filePath: string = path.join(__dirname, "package.json");
 
 // Get path to dist folder
-const distPath = path.join(__dirname, "dist");
+const distPath: string = path.join(__dirname, "dist");
 
 try {
     const fileContent: string = fs.readFileSync(filePath, "utf-8");
-    const pkg = JSON.parse(fileContent);
-   // console.log("JSON Data:", pkg);
 
-    const { name, version, description, author, license } = pkg;
+    const pkg = JSON.parse(fileContent);
+
+    const { name, version, description, author, keywords } = pkg;
+
+    const keywordsString: string = keywords.toString();
+
+    // convert keywords to array of strings
+    const keywordArray: string[] = keywordsString.split(",").map((keyword: string) => keyword.trim());
 
     // Change directory to dist folder
     process.chdir(distPath);
 
-    execSync("ls");
-
-    console.log("name: ", name);
-
     // Run npm init -y to create a package.json file adding the name, version, description, author, and license from the original package.json file
     execSync(`npm init -y`);
 
-    execSync(`npm pkg set name=${name} version=${version} description=${description}`);
+    execSync(`npm pkg set name=${name}`);
 
+    execSync(`npm pkg set version=${version}`);
+
+    execSync(`npm pkg set description="${description}"`);
+
+    execSync(`npm pkg set author="${author}"`);
+
+    keywordArray.forEach((keyword: string, index: number) => {
+        execSync(`npm pkg set keywords[${index}]="${keyword}"`);
+    });
+
+    // Ste type to module
+    execSync(`npm pkg set type=module`);
 
 } catch (error) {
     console.error("Error reading JSON file:", error);
